fc5ae58bf90b4fdbb335e41e874aa3d83c7ea7ae
HarisMI40 <harismaulanaikhsan40@gmail.com>
2023-11-23 16:31:42 +0700

    update hal kelas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('admin.layouts.auth-app')
+@section('title', 'Home')
+@section('content')
+<div>
+    <div class="row align-items-center justify-content-center">
+        <div class="col-md-8 col-lg-6 col-xl-5">
+            <div class="card">
+                <div class="card-body p-4">
+                    <div class="text-center mt-2">
+                        <h5 class="text-primary">Selamat Datang di Aplikasi Pelanggaran Siswa</h5>
+                    </div>
+                    <div class="p-2 mt-4">
+                        <a href="{{ route('admin.auth.login') }}" class="btn btn-block btn-success d-block mb-2">Login Admin</a>
+                        <a href="{{ route('guru.auth.login') }}" class="btn btn-block btn-primary d-block">Login Guru</a>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- end row -->
+</div>
+
+@endsection
